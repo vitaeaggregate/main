d783568371c6fc22c5dd4fa79c4d5174a55137ec
Gustavo Minoru <gminetoma@users.noreply.github.com>
2024-08-27 07:31:26 +0000

    Feat: PostgreSQL connected
    Modules installed:
    
    - psycopg2 -> https://pypi.org/project/psycopg/
    - dj_database_url -> https://pypi.org/project/dj-database-url/
    - python-decouple -> https://pypi.org/project/python-decouple/
    
    Files required:
    
    .env file with DATABASE_URL variable. Example:
    
    DATABASE_URL=postgresql://user:password@database-path:port/database-name
    
    DATABASE_URL=postgresql://postgres:postgres@localhost:5432/malenia
    
    How to configure the database url:
    
    DATABASES = {
        "default": dj_database_url.config(default=config("DATABASE_URL"))
    }
    
    deleted:    .vscode/settings.json -> Don't need it for now.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import dj_database_url
+
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +77,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(default=config("DATABASE_URL"))
 }
 
 
