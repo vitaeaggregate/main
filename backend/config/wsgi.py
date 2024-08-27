73b546942eb10c6b7932611b94fa625757d3c7d4
Gustavo Minoru <gminetoma@users.noreply.github.com>
2024-08-27 09:43:23 +0000

    Fix: Changed psycopg2 to the binary version.

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for config project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``app``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
