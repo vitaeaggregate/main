7a6e198cfe61ea6043e990530371396da6815888
Gustavo Minoru <gminetoma@users.noreply.github.com>
2024-08-27 08:34:37 +0000

    Fix: Files modified to satisfy Vercel deployment
    Resource used:
    
    https://vercel.com/templates/python/django-hello-world

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-598=v7)5v4lu&z$zc^%!26o7x8!=k1^!lh8b5n-l=&bemw+0az"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "config.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.app"
 
 
 # Database
