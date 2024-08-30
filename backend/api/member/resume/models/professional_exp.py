4e948b6154e4309d983098492938e5c3d19d71d4
Brandon <bcook2289@gmail.com>
2024-08-30 05:35:56 +0000

    update: changed professional_exp fields

--- backend/api/member/resume/models/professional_exp.py
+++ backend/api/member/resume/models/professional_exp.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class ProfessionalExp(models.model):
     resume = models.ForeignKey(Header, on_delete=models.CASCADE, blank=False)
-    full_name = models.CharField(blank=False)
     job_title = models.CharField(blank=False)
-    email = models.EmailField()
-    phone_number = models.CharField()
-    address = models.CharField()
-    date_of_birth = models.IntegerField()
-    drivers_license = models.BooleanField()
+    employer = models.CharField(blank=False)
+    city = models.CharField(blank=True)
+    country = models.CharField(blank=True)
+    start_date = models.DateTimeField(null=True)
+    end_date = models.DateTimeField(null=True)
+    description = models.TextField(blank=False)
