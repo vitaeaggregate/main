ed4bf0a11195b99f4570483b35d697c349238148
Minetoma Gustavo <gminetoma@gmail.com>
2024-08-30 08:24:15 +0000

    Feat: New Reference route
    modified:   api/member/urls.py -> To add a new route, insert on the top
    of the views dictionary the route and the view object.

--- backend/api/member/urls.py
+++ backend/api/member/urls.py
@@ -4,39 +4,29 @@ from .resume.views.header import HeaderViewSet
 from .resume.views.personal_info import PersonalInfoViewSet
 from .resume.views.link import LinkViewSet
 from .resume.views.professional_exp import ProfessionalExpViewSet
+from .resume.views.reference import ReferenceViewSet
+
+views = {
+    "personal-infos": PersonalInfoViewSet,
+    "links": LinkViewSet,
+    "professional-exps": ProfessionalExpViewSet,
+    "references": ReferenceViewSet
+}
 
 router = DefaultRouter()
 
-# Two routes here for each table
-
-# Professional Exp Route
-router.register(r"members/(?P<member_pk>[^/.]+)/resumes/(?P<resume_pk>[^/.]+)/professional-exp",
-                ProfessionalExpViewSet, basename="view professional exp from by resume id ")
-router.register(r"members/(?P<member_pk>[^/.]+)/resumes/professional-exp",
-                ProfessionalExpViewSet, basename="view all professional exp from all resumes from a member")
-router.register(r"members/resumes/professional-exp",
-                ProfessionalExpViewSet, basename="view all professional exp")
-
-# Link Route
-router.register(r"members/(?P<member_pk>[^/.]+)/resumes/(?P<resume_pk>[^/.]+)/links",
-                LinkViewSet, basename="view link by resume id ")
-router.register(r"members/(?P<member_pk>[^/.]+)/resumes/links",
-                LinkViewSet, basename="view all links from all resumes from a member")
-
-# Personal Info Route
-router.register(r"members/(?P<member_pk>[^/.]+)/resumes/(?P<resume_pk>[^/.]+)/personal-infos",
-                PersonalInfoViewSet, basename="view personal info by resume id ")
-router.register(r"members/(?P<member_pk>[^/.]+)/resumes/personal-infos",
-                PersonalInfoViewSet, basename="view all personal infos from all resumes from a member")
+for route in views:
+    router.register(r"members/(?P<member_pk>[^/.]+)/resumes/(?P<resume_pk>[^/.]+)/" + route,
+                    views[route], basename="view " + route + " by resume id ")
+    router.register(r"members/(?P<member_pk>[^/.]+)/resumes/" + route,
+                    views[route], basename="view all " + route + " from all resumes from a member")
+    router.register(r"members/resumes/" + route,
+                    views[route], basename="view all " + route)
 
 # Resume Route
 router.register(r"members/(?P<member_pk>[^/.]+)/resumes",
                 HeaderViewSet, basename="view resumes by member id")
-
-# View All from everything here
-router.register(r"members/resumes/links",
-                LinkViewSet, basename="view all links")
-router.register(r"members/resumes/personal-infos",
-                PersonalInfoViewSet, basename="view all personal infos")
 router.register(r"members/resumes", HeaderViewSet, basename="view all resumes")
+
+# Member Route
 router.register(r"members", AccountViewSet)
