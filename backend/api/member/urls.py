8c37f0d73263d6a2d29b7ef3c34c5d30c46f2d50
Brandon <bcook2289@gmail.com>
2024-08-30 06:40:35 +0000

    update: changed routes

--- backend/api/member/urls.py
+++ backend/api/member/urls.py
@@ -4,14 +4,15 @@ from .resume.views.header import HeaderViewSet
 from .resume.views.professional_exp import ProfessionalExpViewSet
 
 router = DefaultRouter()
-router.register(r"members/(?P<member_pk>[^/.]+)/resumes",
-                HeaderViewSet, basename="view resumes by member id")
-router.register(r"members/resumes", HeaderViewSet, basename="view all resumes")
-router.register(r"members", AccountViewSet)
 
 router.register(r"members/(?P<member_pk>[^/.]+)/resumes/(?P<resume_pk>[^/.]+)/professional-exp",
                 ProfessionalExpViewSet, basename="view professional exp from by resume id ")
 router.register(r"members/(?P<member_pk>[^/.]+)/resumes/professional-exp",
                 ProfessionalExpViewSet, basename="view all professional exp from all resumes from a member")
+
+router.register(r"members/(?P<member_pk>[^/.]+)/resumes",
+                HeaderViewSet, basename="view resumes by member id")
 router.register(r"members/resumes/professional-exp",
                 ProfessionalExpViewSet, basename="view all professional exp")
+router.register(r"members/resumes", HeaderViewSet, basename="view all resumes")
+router.register(r"members", AccountViewSet)
