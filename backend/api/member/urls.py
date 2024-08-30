7536ddaab22c7e6d4908e94a0ee88e645e2beb81
ljbroersen <lj.broersen@gmail.com>
2024-08-30 06:39:20 +0000

    feat: changed urls.py and ran migration

--- backend/api/member/urls.py
+++ backend/api/member/urls.py
@@ -8,7 +8,9 @@ router.register(r"members/(?P<member_pk>[^/.]+)/resumes",
                 HeaderViewSet, basename="view resumes by member id")
 router.register(r"members/resumes", HeaderViewSet, basename="view all resumes")
 router.register(r"members", AccountViewSet)
-router.register(r"members/(?P<member_pk>[^/.]+)/resumes/(?P<resume_pk>[^/.]+)/educational-exp",
+router.register(r"members/resumes/educational-exps",
+                EducationExpViewSet, basename="view all educational experience")
+router.register(r"members/(?P<member_pk>[^/.]+)/resumes/educational-exps",
+                EducationExpViewSet, basename="view educational experience by member")
+router.register(r"members/(?P<member_pk>[^/.]+)/resumes/(?P<resume_pk>[^/.]+)/educational-exps",
                 EducationExpViewSet, basename="view all educational experience by resume id")
-router.register(r"members/(?P<member_pk>[^/.]+)/resumes/(?P<resume_pk>[^/.]+)/educational-exp/(?P<id_pk>[^/.]+)",
-                EducationExpViewSet, basename="view educational experience by educational experience id ")
