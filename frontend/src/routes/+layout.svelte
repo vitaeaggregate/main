ad4c4498bea76a031e2ad37dc3c2a9f1c9afc515
Minetoma Gustavo <gminetoma@gmail.com>
2024-08-31 18:38:04 +0000

    Merged with dev branch.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,11 +3,9 @@
 </script>
 
 <slot>
-
-<nav>
-	<a href="/">home</a>
-	<a href="/dashboard">dashboard</a>
-    <a href="/community">community</a>
-</nav>
-
-<slot />
+	<nav>
+		<a href="/">home</a>
+		<a href="/dashboard">dashboard</a>
+		<a href="/community">community</a>
+	</nav>
+</slot>
