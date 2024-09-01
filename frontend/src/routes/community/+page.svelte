8a3646d437bf89483fa654d9b01849cff66b655a
ljbroersen <lj.broersen@gmail.com>
2024-09-01 16:59:50 +0000

    feat: set up Lexical demo environment

--- frontend/src/routes/community/+page.svelte
+++ frontend/src/routes/community/+page.svelte
@@ -1,1 +1,24 @@
-<h1>Community</h1>
+<script>
+	import RichTextComposer from "../../lib/svelte-lexical/src/RichTextComposer.svelte";
+	import "../../lib/svelte-lexical/src/global.css";
+</script>
+
+<main>
+	<h1>Community</h1>
+	<p>
+		Welcome to <a href="https://github.com/umaranis/svelte-lexical/"> svelte-lexical </a>
+		demo built using
+		<a href="https://kit.svelte.dev">SvelteKit</a>
+	</p>
+	<div style="text-align: left;">
+		<RichTextComposer />
+	</div>
+</main>
+
+<style>
+	main {
+		padding: 1em;
+		max-width: none;
+		margin: 0 auto;
+	}
+</style>
