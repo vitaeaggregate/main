f638f862193cfce7229a204ba7e1c31576f62e09
Minetoma Gustavo <gminetoma@gmail.com>
2024-09-01 06:46:29 +0000

    Feat: New Language component

---
+++ frontend/src/lib/components/resume/Language.svelte
@@ -0,0 +1,11 @@
+<script>
+	import InputText from "./InputText.svelte";
+	import TextArea from "./TextArea.svelte";
+
+</script>
+<div class="flex flex-col">
+    <h2>Language</h2>
+    <InputText label="Language"></InputText>
+    <TextArea label="Description"></TextArea>
+    <InputText label="Skill Level"></InputText>
+</div>
