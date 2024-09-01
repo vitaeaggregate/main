d99a368dfde3af0473b9611a7e88d769f40f3032
Minetoma Gustavo <gminetoma@gmail.com>
2024-09-01 06:01:12 +0000

    Fix: Removed placeholder prop from InputDate

--- frontend/src/lib/components/resume/InputDate.svelte
+++ frontend/src/lib/components/resume/InputDate.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	export let label: string = "Input Date";
-	export let placeholder: string = label;
 </script>
 
 <label class="flex flex-col">
 	{label}:
-	<input type="date" {placeholder} class="border-2" />
+	<input type="date" class="border-2" />
 </label>
