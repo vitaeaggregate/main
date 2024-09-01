4a5a2b94fa809cfb678f77bf981302c415d7bc72
ljbroersen <lj.broersen@gmail.com>
2024-09-01 12:09:51 +0000

    feat: onMount fetch request to get resume titles of member. Waiting for endpoint and seed data

--- frontend/src/routes/my/dashboard/+page.svelte
+++ frontend/src/routes/my/dashboard/+page.svelte
@@ -1,20 +1,32 @@
-<script>
+<script lang="ts">
+	import type Header from '$lib/interfaces/resume/Header';
     import { onMount } from 'svelte';
   
-    onMount(async () => {
-      const response = await fetch(URL);
-      const data: Resume[] = await response.json();
-  
-      console.log(data);
+    let resumes: Header[] = [];
+
+onMount(async () => {
+  const response = await fetch(URL);
+  // The URL should include the member id stored in the local storage
+  const dataResume: Header[] = await response.json();
+
+  resumes = dataResume;
     });
   </script>
 
 
 <div class="grid grid-rows-2">
-<div class="m-5"><h1>Dashboard</h1></div>
-<div class="grid grid-cols-3 gap-4">
-<div class="m-5"><h2>User Info</h2></div>
-<div class="m-5"><h2>Resume</h2></div>
-<div class="m-5"><h2>Community</h2></div>
-</div>
+  <div><h1>Dashboard</h1></div>
+  <div class="grid grid-cols-3 gap-20">
+    <div><h2>User Info</h2>
+      <p>Text</p>
+    </div>
+    <div><h2>Resumes</h2>
+      {#each resumes as resume}
+        <p>{resume.title}</p>
+      {/each}
+    </div>
+    <div><h2>Community</h2>
+      <p>Text</p>
+    </div>
+  </div>
 </div>
