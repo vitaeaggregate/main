3e0bcf540123f9f8874771d524e9353a07911047
Minetoma Gustavo <gminetoma@gmail.com>
2024-09-01 06:33:47 +0000

    Feat: Created Project component

---
+++ frontend/src/lib/components/resume/Project.svelte
@@ -0,0 +1,12 @@
+<script>
+	import InputDate from "./InputDate.svelte";
+	import InputText from "./InputText.svelte";
+</script>
+
+<div class="flex flex-col">
+	<InputText label="Title"></InputText>
+	<InputText label="Subtitle"></InputText>
+	<InputDate label="Start Date"></InputDate>
+	<InputDate label="End Date"></InputDate>
+	<InputText label="Description"></InputText>
+</div>
--- frontend/src/routes/my/resumes/+page.svelte
+++ frontend/src/routes/my/resumes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Education from "$lib/components/resume/Education.svelte";
-
+	import Project from "$lib/components/resume/Project.svelte";
 </script>
+
 <h1>Resumes</h1>
 
 <Education></Education>
+<Project></Project>
