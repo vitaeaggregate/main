0f8ad641cfc862bd94218613dc1c1c4cc5e1f7d7
ljbroersen <lj.broersen@gmail.com>
2024-09-01 06:54:46 +0000

    feat: created Award component

---
+++ frontend/src/lib/components/resume/Award.svelte
@@ -0,0 +1,13 @@
+<script>
+	import InputDate from "./InputDate.svelte";
+	import InputText from "./InputText.svelte";
+	import TextArea from "./TextArea.svelte";
+</script>
+
+<div class="flex flex-col">
+	<h2>Award</h2>
+	<InputText label="Title"></InputText>
+	<InputText label="Issuer"></InputText>
+	<InputDate label="Date"></InputDate>
+	<TextArea label="Description"></TextArea>
+</div>
--- frontend/src/lib/components/resume/ProfessionalExp.svelte
+++ frontend/src/lib/components/resume/ProfessionalExp.svelte
@@ -6,11 +6,11 @@
 
 <div class="flex flex-col">
     <h2>Professional Experience</h2>
-        <InputText label="Job Title" placeholder="Put your job title here"></InputText>
-        <InputText label="Employer" placeholder="Put the employer name here"></InputText>
-        <InputText label="City" placeholder="Put the city name here"></InputText>
-        <InputText label="Country" placeholder="Put the name of the country here"></InputText>
+        <InputText label="Job Title"></InputText>
+        <InputText label="Employer"></InputText>
+        <InputText label="City"></InputText>
+        <InputText label="Country"></InputText>
         <InputDate label="Start Date"></InputDate>
         <InputDate label="End Date"></InputDate>
-        <TextArea label="Description" placeholder="Put the description here"></TextArea>
+        <TextArea label="Description"></TextArea>
 </div>
