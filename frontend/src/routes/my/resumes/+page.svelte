434aec3baf6d3d93c445b581b139c6155d5559af
Brandon <bcook2289@gmail.com>
2024-09-01 07:16:20 +0000

    feat: merging

--- frontend/src/routes/my/resumes/+page.svelte
+++ frontend/src/routes/my/resumes/+page.svelte
@@ -1,7 +1,21 @@
 <script>
 	import Education from "$lib/components/resume/Education.svelte";
-
+	import Interest from "$lib/components/Interest.svelte";
+	import Links from "$lib/components/Links.svelte";
+	import PersonalInfo from "$lib/components/PersonalInfo.svelte";
+	import Publication from "$lib/components/Publication.svelte";
+	import Skill from "$lib/components/Skill.svelte";
 </script>
-<h1>Resumes</h1>
 
-<Education></Education>
+<div>
+
+	<h1>Resumes</h1>
+	<Education></Education>
+		
+		<Interest/>
+		<Links/>
+		<PersonalInfo/>
+		<Publication/>
+		<Skill/>
+
+</div>
