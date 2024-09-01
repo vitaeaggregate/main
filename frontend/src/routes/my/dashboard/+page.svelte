d097bf7951923164b07426e24322d9ae1635801c
ljbroersen <lj.broersen@gmail.com>
2024-09-01 10:57:30 +0000

    feat: fixed the navbar, created dashboard grid

--- frontend/src/routes/my/dashboard/+page.svelte
+++ frontend/src/routes/my/dashboard/+page.svelte
@@ -1,1 +1,20 @@
-<h2>Dashboard</h2>
+<script>
+    import { onMount } from 'svelte';
+  
+    onMount(async () => {
+      const response = await fetch(URL);
+      const data: Resume[] = await response.json();
+  
+      console.log(data);
+    });
+  </script>
+
+
+<div class="grid grid-rows-2">
+<div class="m-5"><h1>Dashboard</h1></div>
+<div class="grid grid-cols-3 gap-4">
+<div class="m-5"><h2>User Info</h2></div>
+<div class="m-5"><h2>Resume</h2></div>
+<div class="m-5"><h2>Community</h2></div>
+</div>
+</div>
