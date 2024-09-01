0bf762897f9cc5d35ea2906d0cafb771aab16ad0
ljbroersen <lj.broersen@gmail.com>
2024-09-01 06:15:26 +0000

    feat: created professionalExp component

---
+++ frontend/src/lib/components/resume/ProfessionalExp.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount } from "svelte";
+	import TextArea from "./TextArea.svelte";
+	import InputText from "./InputText.svelte";
+	import InputDate from "./InputDate.svelte";
+    
+    onMount(async () => {
+      
+    });
+  </script>
+
+<div class="professional-exp">
+    <form>
+        <label for="job-title">Job Title</label>
+        <InputText />
+        <label for="employer">Employer</label>
+        <InputText />
+        <label for="city">City</label>
+        <InputText/>
+        <label for="country">Country</label>
+        <InputText/>
+        <label for="start_date">Start Date</label>
+        <InputDate />
+        <label for="end_date">End Date</label>
+        <InputDate />
+        <label for="description">Description</label>
+        <TextArea />
+    </form>
+</div>
