c63fdb7dbe1ee4359421d41257a2a848f3cae4a7
Minetoma Gustavo <gminetoma@gmail.com>
2024-09-02 05:06:25 +0000

    Fix: Resume by member id is up

--- backend/api/firebase_admin/middleware.py
+++ backend/api/firebase_admin/middleware.py
@@ -12,15 +12,19 @@ class FirebaseMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.path.startswith("/"):
+        if request.path.startswith("/admin"):
             return self.get_response(request)
 
         auth_header = request.headers.get("Authorization")
 
+        #TODO DELETE
+        # auth_header = "a a"
+
         if not auth_header:
             return JsonResponse({'status': 'error', 'message': 'Authorization header missing'}, status=status.HTTP_401_UNAUTHORIZED)
 
-        firebase_token = auth_header.split(" ")[1]
+        auth_header = auth_header.split(" ")
+        firebase_token = auth_header[1]
 
         if not firebase_token:
             return JsonResponse({"error": "Token is required"}, status=status.HTTP_400_BAD_REQUEST)
@@ -31,10 +35,16 @@ class FirebaseMiddleware:
             firebase_uid = decode_token["uid"]
             email = decode_token["email"]
 
+            # TODO DELETE
+            # firebase_uid = "I child leg probably others run other. Huge best join offer per."
+            # email = auth_header[2]
+            
             account, created = Account.objects.update_or_create(
                 email=email,
                 defaults={"firebase_uid": firebase_uid}
             )
+
+            print("Hard coded user id: ", account.id)
 
             request.account = AccountSerializer(account).data
 
