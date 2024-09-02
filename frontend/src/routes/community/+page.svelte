0b1c758deab9d526860636879358c723600cf0b1
ljbroersen <lj.broersen@gmail.com>
2024-09-02 16:56:25 +0000

    feat: set up get fetch for comments (however, 401 error)

--- frontend/src/routes/community/+page.svelte
+++ frontend/src/routes/community/+page.svelte
@@ -1,9 +1,24 @@
-<script>
+<script lang="ts">
 	import RichTextComposer from "../../lib/svelte-lexical/src/RichTextComposer.svelte";
 	import "../../lib/svelte-lexical/src/global.css";
+	import type { Comment } from "svelte/types/compiler/interfaces";
+
+  import { onMount } from "svelte";
+
+  let comments: Comment[] = [];
+
+  onMount(async () => {
+	const url = "http://localhost:8000/members/1/resumes/1/comments"; // Construct URL with API endpoint
+
+const response = await fetch(url);
+const commentsData: Comment[] = await response.json();
+comments = commentsData;
+console.log(comments)
+  });
+
 </script>
 
-<main>
+<div class="p-2 m-auto">
 	<h1>Community</h1>
 	<p>
 		Welcome to <a href="https://github.com/umaranis/svelte-lexical/"> svelte-lexical </a>
@@ -13,12 +28,12 @@
 	<div style="text-align: left;">
 		<RichTextComposer />
 	</div>
-</main>
-
-<style>
-	main {
-		padding: 1em;
-		max-width: none;
-		margin: 0 auto;
-	}
-</style>
+	<br/>
+	<h2>Comments</h2>
+	<div>
+	{#each comments as comment}
+	<p>{comment.description}</p>
+	{/each}
+	<p>There is text here</p>
+</div></div>
+	
