b7f053521bd18f90ac6e3d811da9a5028fe940e6
Minetoma Gustavo <gminetoma@gmail.com>
2024-09-03 08:45:34 +0000

    Fix: Passing wrong params to is_owner function

--- backend/api/member/resume/views/header.py
+++ backend/api/member/resume/views/header.py
@@ -22,11 +22,11 @@ class HeaderViewSet(viewsets.ModelViewSet):
         return Header.objects.filter(is_shareable=True)
 
     def create(self, request, *args, **kwargs):
-        if self.is_owner(self, request, Action.CREATE):
+        if self.is_owner(request, Action.CREATE):
             return super().create(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
-        if self.is_owner(self, request, Action.UPDATE):
+        if self.is_owner(request, Action.UPDATE):
             return super().update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
@@ -35,7 +35,7 @@ class HeaderViewSet(viewsets.ModelViewSet):
 
     def is_owner(self, request, action: Action) -> bool:
         member_pk = self.kwargs.get("member_pk")
-        if not member_pk == request.account.get("id"):
+        if not int(member_pk) == request.account.get("id"):
             raise PermissionDenied(
                 "You Don't have the permission to " + action.__str__() + " a resume in the name of another user.")
         return True
