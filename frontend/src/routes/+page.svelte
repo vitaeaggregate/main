f96a7c83652bb4ca6341b6ae762d3bec299c2809
ljbroersen <lj.broersen@gmail.com>
2024-09-04 05:33:33 +0000

    feat: text aligned vertically on landing page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,10 @@
 <script>
 	import { goto } from "$app/navigation";
-
-
 	import { account } from "$lib/store";
-
 </script>
-<main>
-<h1>Vitae Aggregate</h1>
+
+<main class="grid grid-cols-3 ">
+<span class="[writing-mode:vertical-lr] text-7xl">Vitae Aggregate</span>
 
 {#if !$account}<button class="w-20 h-10  bg-slate-200 mt-5" on:click={() => goto("/login/test")}>Login</button>{:else}
 	<button class="w-28 h-10 bg-slate-200 mt-5" on:click={() => goto("/my/dashboard")}>Dashboard
