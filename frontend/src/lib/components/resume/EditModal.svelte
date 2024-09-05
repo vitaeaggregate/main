2bd6230f2edeb0f122c880dae2a7b4d29cea2e87
Minetoma Gustavo <gminetoma@gmail.com>
2024-09-05 22:56:49 +0000

    Feat: Only the skill is being added.

---
+++ frontend/src/lib/components/resume/EditModal.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Button from "$lib/components/Button.svelte"
+	import type { ComponentType } from "svelte";
+
+	export let component: ComponentType;
+	export let value: {};
+	export let title: string = "";
+</script>
+
+<div>
+	<h2>{title}</h2>
+	<svelte:component this={component} bind:value></svelte:component>
+	<Button>Save</Button>
+	<Button>Cancel</Button>
+</div>
--- frontend/src/lib/components/resume/Skill.svelte
+++ frontend/src/lib/components/resume/Skill.svelte
@@ -1,23 +1,58 @@
+<script context="module" lang="ts">
+	export interface Config {
+		readOnly?: boolean;
+		listOnly?: boolean;
+		listLabel?: String;
+	}
+</script>
+
 <script lang="ts">
-	import InputText from "$lib/components/InputText.svelte";
-	import TextArea from "$lib/components/TextArea.svelte";
+	import SkillItem from "$lib/components/resume/SkillItem.svelte";
+	import type Skill from "$lib/interfaces/resume/Skill";
+	import Button from "../Button.svelte";
+	import EditModal from "./EditModal.svelte";
 
-	export let value = {};
+	export let value: Skill | Skill[] = {
+		name: "",
+		description: "",
+		skill_level: ""
+	};
 
-	let inputName: string = "";
-	let inputDescription: string = "";
-	let inputSkillLevel: string = "";
+	export let config: Config = {
+		readOnly: false
+	};
 
-	$: value = {
-		name: inputName,
-		description: inputDescription,
-		skill_level: inputSkillLevel
+	let currentSkill: Skill | null = null;
+
+	const handleRemove = (name: string | undefined) => {
+		if (Array.isArray(value)) value = value.filter((value) => value.name !== name);
+	};
+
+	const handleEdit = (skill: Skill) => {
+		currentSkill = { ...skill };
 	};
 </script>
 
-<div class="flex flex-col">
-	<h2>Skills</h2>
-	<InputText label="Name" bind:value={inputName} />
-	<TextArea label="Description" bind:value={inputDescription} />
-	<InputText label="Skill Level" bind:value={inputSkillLevel} />
-</div>
+{#if currentSkill}
+	<EditModal component={SkillItem} value={currentSkill}></EditModal>
+{/if}
+{#if Array.isArray(value) && value.length}
+	<div class="flex flex-col">
+		{#if config.listLabel}
+			<h2>{config.listLabel}</h2>
+		{/if}
+		{#each value as skill, index (skill.name)}
+			<SkillItem bind:value={skill} readOnly={config.readOnly}></SkillItem>
+			<div>
+				<Button on:click={() => handleEdit(skill)}>Edit</Button>
+				<Button on:click={() => handleRemove(skill.name)}>Remove</Button>
+			</div>
+			<hr />
+		{/each}
+	</div>
+{:else if !Array.isArray(value)}
+	<div class="flex flex-col">
+		<h2>Skill</h2>
+		<SkillItem bind:value readOnly={config.readOnly}></SkillItem>
+	</div>
+{/if}
