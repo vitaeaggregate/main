f781c428b32a5a9230d58ffe93acd3c4f761a6b5
ljbroersen <lj.broersen@gmail.com>
2024-09-14 18:14:26 +0000

    feat: added a drop down menu of some sorts for all the categories (smooth transitioning)

--- frontend/src/lib/components/resume/ResumePreview.svelte
+++ frontend/src/lib/components/resume/ResumePreview.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
-  import type { Resume } from "$lib/interfaces/resume/Resume";
+  import AddressIcon from "$lib/icons/AddressIcon.svelte";
+import EmailIcon from "$lib/icons/EmailIcon.svelte";
+	import PhoneIcon from "$lib/icons/PhoneIcon.svelte";
+import type { Resume } from "$lib/interfaces/resume/Resume";
+import { slide } from 'svelte/transition';
+
   export let resume: Resume;
   export let sectionElement: HTMLElement | null;
+
+  let showSkills = false;
+  let showLanguages = false;
+  let showProjects = false;
+  let showProfessionalExp = false;
+  let showEducation = false;
+	let container;
+
+  function fadeSlide(node, options) {
+		const slideTrans = slide(node, options)
+		return {
+			duration: options.duration,
+			css: t => `
+				${slideTrans.css(t)}
+				opacity: ${t};
+			`
+		};
+	}
+
 </script>
 
 <section bind:this={sectionElement}>
@@ -27,38 +51,46 @@
         font-style: italic;
       }
     </style>
-    <div class="source-sans-3 flex flex-col gap-5">
+
+
+    <div class="source-sans-3 flex flex-col gap-2">
       <div class="flex flex-col gap-2 bg-white rounded-xl p-4">
         <h1 class="lobster-two-bold-italic text-4xl">{resume.personal_info.full_name}</h1>
         <h2 class="text-3xl font-normal"><i>{resume.personal_info.job_title}</i></h2>
-      </div>
+        <div class="flex flex-row"><EmailIcon/><span class="pl-2"> <a href={"mailto:" + resume.personal_info.email}>{resume.personal_info.email}</a></span></div>
+        <div class="flex flex-row"><PhoneIcon /><span class="pl-2"> {resume.personal_info.phone_number}</span></div>
+        <div class="flex flex-row"><AddressIcon /><span class="pl-2">{resume.personal_info.address}</span></div>
       <div>
         {#if resume.links.length}
-          <div class="flex gap-3 bg-white rounded-xl p-4">
-            <a href={"mailto:" + resume.personal_info.email}>{resume.personal_info.email}</a>
+          <div class="flex gap-3">
             {#each resume.links as link (link.id)}
               <a href={link.url}>{link.title?.slice(0, 15)}</a>
             {/each}
-          </div>
+          </div>      
         {/if}
-        <span class="bg-white flex gap-3 rounded-xl p-4">{resume.personal_info.address}</span>
+      </div>
       </div>
       {#if resume.skills.length}
-        <div class="flex flex-col gap-3  bg-white rounded-xl p-4">
-          <h3 class="w-fit border-b-2 border-black">Skills</h3>
-          <div class="grid grid-cols-4 gap-10">
+      
+        <div bind:this={container} class="flex flex-col gap-3 bg-white rounded-xl p-4">
+          <button on:click={() => showSkills = !showSkills }><h3 class="w-fit border-b-2 border-black">Skills</h3></button>
+          {#if showSkills}
+          <div transition:fadeSlide="{{duration: 300}}" class="grid grid-row-4 gap-10">
             {#each resume.skills as skill (skill.id)}
-              <div>
+              <div >
                 <p><strong>{skill.name}</strong></p>
                 <p>{skill.description}</p>
               </div>
             {/each}
           </div>
+          {/if}
         </div>
       {/if}
       {#if resume.languages.length}
-        <div class="flex flex-col gap-3  bg-white rounded-xl p-4">
-          <h3 class="w-fit border-b-2 border-black">Languages</h3>
+        <div bind:this={container} class="flex flex-col gap-3 bg-white rounded-xl p-4">
+          <button on:click={() => showLanguages = !showLanguages }><h3 class="w-fit border-b-2 border-black">Languages</h3></button>
+          {#if showLanguages}
+          <div transition:fadeSlide="{{duration: 300}}">
           <ul class="flex gap-10">
             {#each resume.languages as language (language.id)}
               <li>
@@ -67,11 +99,14 @@
             {/each}
           </ul>
         </div>
+          {/if}
+        </div>
       {/if}
       {#if resume.projects.length}
-        <div class="flex flex-col gap-3  bg-white rounded-xl p-4">
-          <h3 class="w-fit border-b-2 border-black">Projects</h3>
-          <div class="flex flex-col gap-5">
+        <div bind:this={container} class="flex flex-col gap-3 bg-white rounded-xl p-4">
+          <button on:click={() => showProjects = !showProjects }><h3 class="w-fit border-b-2 border-black">Projects</h3></button>
+          {#if showProjects}
+          <div transition:fadeSlide="{{duration: 300}}" class="flex flex-col gap-5">
             {#each resume.projects as project (project.id)}
               <div class="flex flex-col gap-2">
                 <div class="grid grid-cols-3">
@@ -86,12 +121,14 @@
               </div>
             {/each}
           </div>
+          {/if}
         </div>
       {/if}
       {#if resume.professional_exps.length}
-        <div class="flex flex-col gap-3  bg-white rounded-xl p-4">
-          <h3 class="w-fit border-b-2 border-black">Professional Experiences</h3>
-          <div class="flex flex-col gap-5">
+        <div bind:this={container} class="flex flex-col gap-3 bg-white rounded-xl p-4">
+          <button on:click={() => showProfessionalExp = !showProfessionalExp }><h3 class="w-fit border-b-2 border-black">Professional Experiences</h3></button>
+          {#if showProfessionalExp}
+          <div transition:fadeSlide="{{duration: 300}}" class="flex flex-col gap-5">
             {#each resume.professional_exps as professional_exp (professional_exp.id)}
               <div class="flex flex-col gap-2">
                 <div class="grid grid-cols-3">
@@ -106,11 +143,13 @@
               </div>
             {/each}
           </div>
+          {/if}
         </div>
       {/if}
       {#if resume.educations.length}
-        <div class="flex flex-col gap-3  bg-white rounded-xl p-4">
-          <h3 class="w-fit border-b-2 border-black">Education and Courses</h3>
+        <div bind:this={container} class="flex flex-col gap-3  bg-white rounded-xl p-4">
+          <button on:click={() => showEducation = !showEducation }><h3 class="w-fit border-b-2 border-black">Education and Courses</h3></button>
+          {#if showEducation}
           <div class="flex flex-col gap-5">
             {#each resume.educations as education (education.id)}
               <div class="flex flex-col gap-2">
@@ -125,6 +164,7 @@
               </div>
             {/each}
           </div>
+          {/if}
         </div>
       {/if}
     </div>
